8b14f6c283397d83a4896811f9d7035ff9c3db90
Juan Cruz Geiersberg <juancruzgeiersberg@gmail.com>
2022-10-10 04:31:35 -0300

    Eliminado de código comentado que no se utilizaba

--- js/js-index.js
+++ js/js-index.js
@@ -3,23 +3,6 @@ const FOOTER_BAR = document.querySelector('.footer-page');
 const SESION_CLOSE = document.getElementById('close-session');
 const LOG = document.getElementById('log');
 
-/*let sticky = MENU_BAR.offsetTop;*/
-
-/* Assigning a function to the onscroll event of the window. */
-/*window.onscroll = function() {changeMenuStricky()};*/
-
-/**
- * If the page is scrolled down more than the height of the menu bar, add the class "fixed-bar" to the
- * menu bar and the menu button. Otherwise, remove the class "fixed-bar" from the menu bar and the menu
- * button.
- */
-/*function changeMenuStricky() {
-    if (window.pageYOffset > sticky) {
-        MENU_BAR.classList.add("fixed-bar");
-    } else {
-        MENU_BAR.classList.remove("fixed-bar");
-    }
-  }*/
 
 
 function verifySession(){
